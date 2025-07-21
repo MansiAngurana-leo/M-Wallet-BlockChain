87956c863beb09e63c4268ad01663c0b175d3377
Mukesh Sharma <mukesh.sharma@antiersolutions.com>
2025-07-21 18:10:18 +0530

    chore: update env for Aiven MySQL

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,7 +9,7 @@ import { WalletModule } from './wallet/wallet.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT ?? '3306', 10),
+      port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
